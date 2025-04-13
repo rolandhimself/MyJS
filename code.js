837ee0c2d43fc9b0bff41334412838ab8843d1ba
Roland Appiah <romappiah@st.ug.edu.gh>
2025-04-13 22:04:02 +0000

    Exercise 29 added

--- code.js
+++ code.js
@@ -517,3 +517,29 @@ while(continu){
 }
 console.log("Exiting Calculator... ");
 console.log ("          GOODBYE        ");
+
+//Exercise 29
+function isLeap(year){
+    if (year % 4 === 0){
+        if (year % 100 === 0){
+            if (year % 400 === 0){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;}
+    }
+
+function daysInMonth(year, month){
+  monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (isLeap(year)===true){
+    monthDays[1]+=1;}
+  return monthDays[month-1];
+}
+
+console.log(`The number of days is ${daysInMonth(2022,2)}`);
